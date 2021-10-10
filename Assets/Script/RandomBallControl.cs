4d4c6f1fa9d822e4ac34d9f4cebeef94ebd81068
Filzi Muhaimin Manan <63395727+darkcaster24-byte@users.noreply.github.com>
2021-10-10 15:30:29 +0700

    problem-3

---
+++ Assets/Script/RandomBallControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBallControl : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+
+    public float xInitialForce;
+    public float yInitialForce;
+
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        PushBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PushBall()
+    {
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+        float xRandomInitialForce = Random.Range(-xInitialForce, xInitialForce);
+
+        rigidBody2D.AddForce(new Vector2(xRandomInitialForce, yRandomInitialForce));
+  
+    }
+}
