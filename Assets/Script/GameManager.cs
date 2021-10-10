8e803ab6e81492d99e20b7d5dbc880e3cbe30973
Filzi Muhaimin Manan <63395727+darkcaster24-byte@users.noreply.github.com>
2021-10-10 17:57:38 +0700

    problem-6

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<SmallBox> smallBox;
+    public int smallBoxCount;
+    public int smallBoxCounter=0;
+
+    private List<GameObject> spawnedSmallBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnedSmallBox = new List<GameObject>();
+        while(smallBoxCounter<=smallBoxCount)
+        {
+            float randomPositionx = Random.Range(-8.7f , 8.7f);
+            float randomPositiony = Random.Range(-4.8f , 4.8f);
+            SpawnSmallBox(randomPositionx, randomPositiony);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void SpawnSmallBox(float posx, float posy){
+        GameObject newSmallBox = Instantiate(smallBox[Random.Range(0, smallBox.Count)].gameObject, transform);
+
+        newSmallBox.transform.position = new Vector2(posx, posy);
+        spawnedSmallBox.Add(newSmallBox);
+        smallBoxCounter++;
+    }
+}
---
+++ Assets/Script/SmallBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallBox : MonoBehaviour
+{
+    // [SerializeField]
+    // private GameManager gameManager;
+    // private void OnTriggerEnter2D(Collider2D col) {
+    //     string tag = col.gameObject.tag;
+    //     if(tag == "Ball"){
+    //         Destroy(gameObject);
+    //         gameManager.smallBoxCounter--;
+    //     }
+    // }
+}
