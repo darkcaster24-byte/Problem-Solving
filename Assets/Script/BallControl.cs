6555a8e7df7cb27514a1a933954060a6e642581e
Filzi Muhaimin Manan <63395727+darkcaster24-byte@users.noreply.github.com>
2021-10-10 14:58:00 +0700

    problem-2

---
+++ Assets/Script/BallControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+ 
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        PushBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PushBall()
+    {
+        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+        float xRandomInitialForce = Random.Range(-xInitialForce, xInitialForce);
+
+        rigidBody2D.AddForce(new Vector2(xRandomInitialForce, yRandomInitialForce));
+  
+    }
+}
